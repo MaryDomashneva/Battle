f24bcdcb74c293b390f2a5d0825c602dbb229214
marydomashneva <mary.domashneva@gmail.com>
2018-04-05 16:18:45 +0100

    Using Web_helper to refactor

--- spec/features/app_features_spec.rb
+++ spec/features/app_features_spec.rb
@@ -1,11 +1,12 @@
 require './app'
 
-describe "the page contents", type: :feature do
-  it "filles players names" do
-    visit('/')
-    fill_in(:Player_1, with: 'Mary')
-    fill_in(:Player_2, with: 'Irbe')
-    click_button 'Submit'
+describe 'the page contents', type: :feature do
+  it 'filles players names' do
+    sign_in_and_play
     expect(page).to have_content('Mary vs. Irbe')
+  end
+  it 'shows Player_2 Hit Points' do
+    sign_in_and_play
+    expect(page).to have_content('Irbe: 60HP')
   end
 end
