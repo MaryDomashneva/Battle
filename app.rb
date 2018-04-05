959e1c28c5d948fd2e6ef8c630b8d4dc2c9b751d
marydomashneva <mary.domashneva@gmail.com>
2018-04-05 15:47:17 +0100

    POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -2,18 +2,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
-    'Hello world!'
-  end
+
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
-  post '/names' do
-    @Player_1 = params[:Player_1]
-    @Player_2 = params[:Player_2]
+  get '/play' do
+    @Player_1 = session[:Player_1]
+    @Player_2 = session[:Player_2]
     erb :play
   end
+
+  post '/names' do
+    session[:Player_1] = params[:Player_1]
+    session[:Player_2] = params[:Player_2]
+    redirect '/play'
+  end
+
   run! if app_file == $0
 end
--- spec/features/app_features_spec.rb
+++ spec/features/app_features_spec.rb
@@ -1,15 +1,11 @@
 require './app'
 
 describe "the page contents", type: :feature do
-  it "displays 'Hello world!'" do
-    visit '/'
-    expect(page).to have_content 'Hello world!'
-  end
   it "filles players names" do
     visit('/')
     fill_in(:Player_1, with: 'Mary')
     fill_in(:Player_2, with: 'Irbe')
     click_button 'Submit'
-    expect(page).to have_content 'Mary vs. Irbe'
+    expect(page).to have_content('Mary vs. Irbe')
   end
 end
